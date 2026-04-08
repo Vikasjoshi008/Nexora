7b235caaf13777f975c02f20ee589470aaa86be2
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-04-08 23:12:55 +0530

    fix;: remove the set headers and cors realated functions

--- Backend/server.js
+++ Backend/server.js
@@ -18,28 +18,10 @@ app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // ✅ THIS FIXES YOUR ERROR
-  }
-
-  next();
-});
-
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
