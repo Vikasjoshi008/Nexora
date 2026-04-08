f399b870577c6877546d552ffabebdb98d5b5529
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-04-08 12:57:42 +0530

    feat: place routes and cors in right place

--- Backend/server.js
+++ Backend/server.js
@@ -5,45 +5,45 @@ dotenv.config();
 import cors from "cors";
 import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
-import authRoutes from "./routes/auth.js"
-
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(
+  cors({
+    origin: ["https://nexora-one.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
-
-app.use(cors({
-  origin: ["https://nexora-one.vercel.app","http://localhost:5173"],
-  credentials: true,
-}));
-
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
   next();
-});
-
-app.use("/api/auth", authRoutes);
-app.use("/api", chatRoutes);
-
-app.listen(PORT, () => {
-  console.log("server is runnig on port",PORT);
-  connectDB();
-});
-
-app.get("/", (req, res)=> {
-  res.send("This is root");
 });
 
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 
+app.get("/", (req, res) => {
+  res.send("This is root");
+});
 
-const connectDB = async() => {
+app.use("/api/auth", authRoutes);
+app.use("/api", chatRoutes);
+
+app.listen(PORT, () => {
+  console.log("server is runnig on port", PORT);
+  connectDB();
+});
+
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected With Database!");
   } catch (err) {
-    console.log("failed to connect with db",err);
+    console.log("failed to connect with db", err);
   }
-}
+};
